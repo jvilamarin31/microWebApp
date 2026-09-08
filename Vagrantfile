38c3856045dc9592f5d77882031ff8e324396b54
Katar012 <juandandlinda@gmail.com>
2026-09-07 23:10:40 -0500

    instalacion de docker desde vagrantfile

--- Vagrantfile
+++ Vagrantfile
@@ -2,15 +2,19 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
+  config.vm.provider :libvirt do |libvirt|
+    libvirt.driver = "kvm"
+  end
 
   config.vm.define :servidorWeb do |servidorWeb|
-    servidorWeb.vm.box = "bento/ubuntu-22.04"
+    servidorWeb.vm.box = "generic/ubuntu2204"
+    servidorWeb.vm.hostname = "servidorWeb"
     servidorWeb.vm.network :private_network, ip: "192.168.56.3"
+
     servidorWeb.vm.provision "file", source: "frontend", destination: "/home/vagrant/frontend"
     servidorWeb.vm.provision "file", source: "microUsers", destination: "/home/vagrant/microUsers"
     servidorWeb.vm.provision "file", source: "microProducts", destination: "/home/vagrant/microProducts"
     servidorWeb.vm.provision "file", source: "microOrders", destination: "/home/vagrant/microOrders"
     servidorWeb.vm.provision "shell", path: "script.sh"
-    servidorWeb.vm.hostname = "servidorWeb"
   end
 end
--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 # Install MySQL
 echo "Installing MySQL"
+sudo apt update -y
 
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
-sudo apt update
 sudo apt install mysql-server -y
 sudo systemctl start mysql.service
 
@@ -29,3 +29,28 @@ pip3 install Flask-MySQLdb
 pip install Flask-SQLAlchemy
 pip3 install requests
 pip3 install python-dotenv
+
+# Install Docker and Docker Compose
+echo "Installing Docker and Docker Compose..."
+sudo apt install ca-certificates curl gnupg -y
+
+# Agregar la clave GPG oficial de Docker
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Configurar el repositorio de Docker
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Instalar Docker Engine, CLI, Containerd y Docker Compose
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Habilitar e iniciar el servicio de Docker
+sudo systemctl enable --now docker
+
+# Permitir ejecutar Docker sin sudo para el usuario vagrant
+sudo usermod -aG docker vagrant
